583bce8d4a077d5d1483b57304fbfa599d6f3b4d
Rahul <pacharne.rahul92@gmail.com>
2021-08-28 09:16:20 +0530

    Registration form

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,9 +7,15 @@ class User(db.Model):
     lastname = db.Column(db.String(64), index=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    default_currency = db.Column(db.String(120), index=True, unique=True)
+    default_currency = db.Column(db.String(120))
     img_name = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,29 @@
 from app import app
+from flask import render_template, flash, redirect,url_for
+from flask_login import current_user, login_user, login_required
+from flask_login import logout_user
+from app.models import User
+from flask import request
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello John"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    #if current_user.is_authenticated:
+        #return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(firstname = form.firstname.data, lastname = form.lastname.data ,default_currency = form.default_currency.data, username=form.username.data, email=form.email.data,)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
