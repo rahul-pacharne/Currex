7f04d81190787764af4f5d4f2c9a4e48fc7152f9
Rahul <pacharne.rahul92@gmail.com>
2021-08-28 07:47:58 +0530

    database set up with users table

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(64), index=True)
+    lastname = db.Column(db.String(64), index=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    default_currency = db.Column(db.String(120), index=True, unique=True)
+    img_name = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
